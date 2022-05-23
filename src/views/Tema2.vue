271a39f612d1010351b68449b191be63dada91ae
AndresDiazPinto <andresvis565@gmail.com>
2022-05-23 01:16:16 -0500

    tema 2-3
    Avance temas 2 y 3

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -7,6 +7,135 @@
         span 2
       h1 Normatividad de seguridad de la información para contactar al cliente 
     
+    .row.mb-5         
+      .col-12.col-lg-4.mb-3
+        figure
+          img(src='@/assets/curso/tema2/t2-1.png' alt='AvatarTop')
+      .col-12.col-lg-8
+        p Ahora se verá más acerca de la normatividad en seguridad de la información. Según la Norma ISO 27002, las empresas deben tomar medidas de seguridad para proteger los datos de los clientes, pues ellos son tan importantes como los productos y servicios que se estén ofertando; la protección de la información es el pilar fundamental de las empresas para brindar la confianza necesaria a sus clientes, a la marca y para captar nuevas oportunidades de negocios.
+        p.bgr_5.p-4 En Colombia, la entidad que regula y vigila el tratamiento de los datos de los usuarios es el Ministerio de las TIC que aplica los estándares de la Norma ISO 27002 para tal fin. La norma ISO 27002 (anteriormente denominada ISO 17799) es un estándar para la seguridad de la información que ha publicado la Organización Internacional de Normalización y la Comisión Electrotécnica Internacional; la versión más reciente de la norma es la ISO 27002:2013.
+           
+    p.mb-3 La norma ISO 27002 se debe implementar elaborando y manteniendo un inventario de activos de información, donde se muestren los propietarios de los activos con todos sus detalles, se debe utilizar un código de barras que facilite las tareas para la realización de los inventarios y vincular los equipos de tecnología de la información TI, que ingresan y salen de las instalaciones y los empleados responsables de ellos.
+    p.mb-5 Para comprender más este tema, en el siguiente video se ofrecen los elementos generales de la seguridad de la información digital o ciberseguridad.
+
+    .bg_grad-01
+      .row.justify-center.pt-5.pb-5
+        .col-12.col-md-10
+          figure
+            .video
+              iframe(width="560" height="315" src="https://www.youtube.com/embed/7DCfBZAsU0g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+    p Existen cinco prácticas muy utilizadas por las empresas para garantizar la protección de datos de los clientes, a saber:
+
+    .titulo-sexto.color-acento-contenido.mb-5
+      h5 Figura 1
+      span <em>Prácticas para la protección de datos</em>
+    
+    .row.justify-center.mb-5
+      .col-12.col-lg-6
+        figure
+          img(src='@/assets/curso/tema2/t2-2.svg' alt='AvatarTop')
+    
+    p.mb-5 A continuación, se desarrolla cada una de las prácticas que garantizan la protección de los datos de los clientes:
+
+    TabsB.color-acento-contenido.mb-5
+      .p-3.p-lg-5(titulo="Inclusión familiar" :icono="require('@/assets/curso/tema2/t2-3.svg')")
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 Medios electrónicos certificados
+            p Para garantizar la protección de datos tanto del cliente como de la empresa se deben ejecutar acuerdos y aprobaciones usando una tecnología segura, en la actualidad puede realizarse desde un computador, un celular o una tableta.
+            p Se recomienda la utilización de herramientas electrónicas rápidas y seguras, como por ejemplo la firma electrónica que permite cargar y enviar archivos protegidos evitando que otros los firmen, ya que es una tecnología vanguardista de criptografía de datos que ofrece una mayor protección y respeta las exigencias del mercado. De esta manera se estarán protegiendo los datos del cliente y de la empresa y brindando un excelente servicio en las transacciones electrónicas y dejar constancia de la veracidad del firmante.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/tema2/t2-8.png', alt='Texto que describa la imagen')
+      .p-3.p-lg-5(titulo="Inclusión familiar" :icono="require('@/assets/curso/tema2/t2-4.svg')")
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 Copia de seguridad
+            p Se crea una copia <em>"backup"</em>, que es una copia de seguridad de los datos que se elijan desde el lugar donde se esté, ya sea desde el computador, celular, tableta o cualquier otro dispositivo del que se disponga.
+            p Lo importante es contar con una mayor seguridad de toda la información para luego disponer de ella a cualquier hora del día durante todo el año y poder recuperarla en caso de que sea necesario.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/tema2/t2-8-1.png', alt='Texto que describa la imagen')
+      .p-3.p-lg-5(titulo="Inclusión familiar" :icono="require('@/assets/curso/tema2/t2-5.svg')")
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 Carpetas con contraseñas
+            p Para mejorar la seguridad de los datos del cliente se puede personalizar a cada uno, organizándolos en carpetas individuales con claves específicas a las que solo tengan acceso y las conozca el personal autorizado para esta función.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/tema2/t2-8-2.png', alt='Texto que describa la imagen')
+      .p-3.p-lg-5(titulo="Inclusión familiar" :icono="require('@/assets/curso/tema2/t2-6.svg')")
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 <em>Backup</em> en la nube
+            p El cloud storage (almacenamiento en la nube) es un servicio tecnológico creado para obtener mayor espacio de almacenamiento, caracterizado por fácil acceso a la información y la protección de los datos del cliente.
+            p Hace algunos años la información la guardábamos en dispositivos poco seguros, disquetes, discos ZIP, USB, DVD, CD o discos portátiles que, con el tiempo, algunos de estos se dañaban y se perdía la información o peor aún si se perdían podían caer en manos no deseadas.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/tema2/t2-8-3.png', alt='Texto que describa la imagen')
+      .p-3.p-lg-5(titulo="Inclusión familiar" :icono="require('@/assets/curso/tema2/t2-7.svg')")
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 Centralización de la información 
+            p Gracias a que se pueden centralizar los datos en la nube, se evita que estén dispersos en varios programas con diferentes métodos de autenticación y de acceso.
+            p Es muy importante preservar tanto los datos del cliente como también las claves de acceso, que permanezcan en un solo lugar hace más fácil y práctico el trabajo.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/tema2/t2-8-4.png', alt='Texto que describa la imagen')
+    
+    p.mb-5 Según cifras del DANE, en el 2021 aumentaron en un 30% los ciberataques en Colombia.
+
+    .titulo-sexto.color-acento-contenido.mb-5
+      h5 Figura 2
+      span <em>Ciberataques en Colombia</em>
+    
+    figure
+      img(src='@/assets/curso/tema2/t2-9.svg', alt='Texto que describa la imagen')
+    
+    div.bgr_6.my-3.d-flex.mb-5
+      p.my-3.px-3  <b>Nota.</b> Datos tomados de Fiscalía General de la Nación (2021).
+
+    h3 Tecnología segura
+
+    p Toda empresa u organización debe tener herramientas de seguridad digital para proteger los datos del cliente, esto le ofrecerá la tranquilidad y seguridad ya que la tecnología segura no permite que software malicioso ingrese y robe los datos que tenga de su cliente interno y su cliente externo, algunas de estas herramientas son:
+
+    SlyderD.mb-5(:datos="datosSlyder")
+
+    p.mb-3 Las empresas tuvieron que cambiar su estructura e invertir en seguridad digital para la protección de sus datos y los de sus clientes ante la enorme transformación digital que vive el mundo. Como las empresas deben tener seguros sus procesos para captar potenciales clientes, esto no solo debe hacerse en términos digitales, sino también jurídicos lo que le generará confianza al cliente.
+    p.mb-5 La utilización de medidas jurídicas para la protección de la información es una de las técnicas más antiguas y efectivas dentro del ámbito laboral, y funcionan de la siguiente manera:
+
+    .row.mb-5
+      .col-12.bgr_7.justify-center
+        h4.mb-0.p-3 Medidas jurídicas
+      .col-12.col-lg-6.bgr_5
+        div.p-5
+          .bg_grad-02
+            figure.mb-3.p-3
+              img.img320(src='@/assets/curso/tema2/t2-13.svg', alt='Texto que describa la imagen')
+          h3 Físicamente
+          p Son documentos tipificados que describen responsabilidades y prohibiciones mediante un número no definido de cláusulas para la protección de información y violación de datos.
+      .col-12.col-lg-6.bgr_3
+        div.p-5
+          .bg_grad-02
+            figure.p-3.justify-end
+              img.img320(src='@/assets/curso/tema2/t2-14.svg', alt='Texto que describa la imagen')
+          h3 Digitalmente
+          p Son soluciones de cifrado de datos que bloquean el acceso a terceros, evitando que cualquier persona pueda consultar la información garantizando la confidencialidad de los datos del cliente.
+
+    .bg_grad-03
+      .row.justify-center
+        .col-12.col-lg-3
+          figure
+            img(src='@/assets/curso/tema2/t2-15.svg', alt='Texto que describa la imagen')
+        .col-12.col-lg-7.align-items-center
+          div.p-3.p-lg-0
+            h4.colr Recomendación     
+            p.colr_1.mb-0 Se recomienda a todos los departamentos empresariales implementar todas las medidas de seguridad posibles, asimismo, hacer fluir la información de términos y condiciones organizacionales lo que afianzará las funciones de sus trabajadores para alcanzar un desempeño productivo y seguro de cara al cliente.
+  
+    
+
+
 
 </template>
 
@@ -14,7 +143,26 @@
 export default {
   name: 'Tema2',
   data: () => ({
-    // variables de vue
+    datosSlyder: [
+      {
+        titulo: '<em>Software</em> de monitoreo a distancia',
+        texto:
+          'La herramienta de monitoreo remoto permite emitir diagnóstico y recopilar la información de los diferentes <em>software</em> y <em>hardware</em> que logren resolver cualquier eventualidad que pueda presentarse manteniendo la red en constante vigilancia para asegurar un correcto funcionamiento.',
+        imagen: require('@/assets/curso/tema2/t2-10.png'),
+      },
+      {
+        titulo: 'Escáner de vulnerabilidades',
+        texto:
+          'Este sistema está hecho para detectar, evaluar y gestionar cualquier fragilidad que pueda existir en el sistema de forma que se puedan identificar y priorizar los puntos vulnerables de seguridad para disminuir y eliminar las probabilidades de engaño <em>(phishing)</em> y fraude de información confidencial.',
+        imagen: require('@/assets/curso/tema2/t2-11.png'),
+      },
+      {
+        titulo: 'Servidor <em>proxy</em>',
+        texto:
+          'Es un instrumento o herramienta digital que sirve de intermediario entre el dispositivo y el ordenador que se tenga conectado al Internet o servidor al que se esté accediendo, se puede utilizar para bloquear sitios web que se consideren peligrosos o prohibidos para las empresas o instituciones educativas, pues evita que se filtre la información inadecuada.',
+        imagen: require('@/assets/curso/tema2/t2-12.png'),
+      },
+    ],
   }),
   mounted() {
     this.$nextTick(() => {
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -7,6 +7,285 @@
         span 3
       h1 Portafolio de servicios
 
+    .row
+      .col-12.col-lg-5.mb-3
+        figure
+          img(src='@/assets/curso/tema3/t3-1.png', alt='Texto que describa la imagen')
+      .col-12.col-lg-7
+        p.mt-lg-5 Las condiciones del mercado han cambiado, anteriormente un empresario solo tenía como herramienta para atender al cliente su mirada para verlo a los ojos y su mano para estrecharla; las condiciones del mercado y los avances tecnológicos ofrecen un mundo de posibilidades para contactar al cliente, no solo física, sino virtualmente lo cual les permite ampliar sus expectativas de negocios ampliando la oferta y optimizando recursos.
+
+    separador
+
+    #t_3_1.titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 3.1 Elementos
+
+    p.mb-3 Un portafolio bien estructurado, de acuerdo con el experto Alberto Cajal, llega a convertirse en un aliado fiel si es utilizado como medio para llegar al cliente, con el propósito de demostrar un orden de lo que es la empresa y mostrar todo su interior para darse a conocer de forma externa. Un portafolio se utiliza a manera de interacción con el cliente, ya que el documento le permite resolver inquietudes que pueda tener del producto o servicio que ofrece la empresa (Cajal A., 2020).
+    p.mb-5 Lo siguiente es lo que necesita un portafolio para cumplir con su objetivo:
+
+    .titulo-sexto.color-acento-contenido.mb-5
+      h5 Figura 1
+      span <em>Elementos básicos de un portafolio</em>
+
+    .row
+      .col-12.col-lg10
+        .tabla-a.color-acento-contenido.mb-5 
+          table
+            thead
+              tr
+                th Elemento
+                th Recomendación
+                th Ítems
+            tbody
+              tr
+                td Información muy detallada de la empresa.
+                td Debe aparecer no solo su historia, sino también sus principales clientes y proveedores, esto le ofrece confianza al cliente.
+                td 
+                  ul.lista-ul
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Historia de la empresa.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Antecedentes.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Antecedentes.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Fecha de creación.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Tiempo en el mercado.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Socios.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Clientes.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Misión.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Visión.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Objetivos.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Propuesta de valor.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Valores organizacionales.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Estructura de la empresa.
+              tr
+                td Información del producto.
+                td Debe especificar sus componentes y principales características, también los precios y ofertas por compra.
+                td 
+                  ul.lista-ul
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Clasificación de los productos o servicios.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Producto o servicio específico.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | La marca de los productos o servicios.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Nombre del producto o servicio.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Características del producto o servicio: Tamaño, forma, color, etc.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Empaque y etiquetado: fecha de vencimiento, recomendaciones de almacenamiento, etc.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Funciones y usos del producto o servicio: para qué sirve, cuáles son sus usos, etc.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Amplitud, extensión y presentación de la línea del producto: número de líneas, cantidad de productos que integran el portafolio, versiones del producto, etc.
+              tr
+                td Información de la negociación.
+                td Se debe especificar las formas de pago y los tiempos de pago en el evento que se venda a crédito y al por mayor.
+                td 
+                  ul.lista-ul
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Valor de los productos.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Valor de los servicios.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Valor por unidad.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Valor al por mayor (docenas).
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Descuentos por pronto pago.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Días de descuentos.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Ofertas especiales.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Formas de pago: efectivo, tarjetas débito o crédito
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Recaudo electrónico: diferentes canales como Efecty, Nequi, Daviplata, etc.
+              tr
+                td Información de contacto.
+                td Se Debe especificar todas las formas por las cuales se puedan comunicar los clientes
+                td 
+                  ul.lista-ul
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Página web.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Correo electrónico.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Blog.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Teléfonos fijos.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Teléfonos celulares.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | WhatsApp.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Instagram
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Twitter.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | <em>Call Center</em> (centro de llamadas).
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Fax.
+                    li 
+                      i.fas.fa-check-circle.colr_2
+                      | Ubicación o dirección de la empresa y sucursales si las tiene. 
+    
+    separador
+
+    #t_3_2.titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 3.2 Características
+
+    p.mb-3 Los portafolios de productos y servicios cuentan con algunas características tales como identificar las metas, plantear las estrategias de ventas, recopilar los productos y servicios ofertados, identificar las perspectivas de crecimiento y la planeación de objetivos.
+    p.mb-5 Los portafolios de productos y servicios tienen unas características que se describen a continuación:
+
+    .row.mb-5
+      .col-12.col-lg-3
+        figure
+          img(src='@/assets/curso/tema3/t3-2.svg', alt='Texto que describa la imagen')
+      .col-12.col-lg-9
+        TabsA.color-acento-botones.mb-5
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Reseña histórica de la empresa.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-3.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10    
+                p.mb-0 Cuándo fue fundada, nombre de los propietarios y logros a través de la historia.
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Visión.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-4.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10    
+                p.mb-0 Se plantea en años lo que se desea lograr, hay que tener en cuenta la tecnología y la globalización para no quedar mal con nuestros clientes en caso de que no se logre cumplir.
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Objetivos de nuestra empresa.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-5.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10    
+                p.mb-0 Especifica lo que se quiere alcanzar durante el tiempo de la visión de la empresa.
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Productos.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-5.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10    
+                p.mb-0 Debe tener la información necesaria para que el cliente conozca las características y bondades del producto.
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Servicios.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-6.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10    
+                p.mb-0 Igual a los productos debe tener la información necesaria para dar claridad de lo que se está ofertando.
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Garantías.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-7.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10    
+                p.mb-0 Es el tiempo o fecha hasta la cual la empresa responde por mal funcionamiento o daños que sufra el producto o servicio.
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Socios.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-8.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10   
+                p.mb-0 Son las personas propietarias o inversionistas de la empresa.
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Proveedores.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-9.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10    
+                p.mb-0 Son las personas o empresas que surten la materia prima o productos terminados a la empresa.
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Respaldos.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-10.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10   
+                p Son las personas o empresas que surten la materia prima o productos terminados a la empresa.
+                p.mb-0  Este apartado se refiere a los respaldos tanto bancarios como de empresas que estarían dispuestas a responder por la empresa en algún evento adverso, o sea las alianzas que tiene.
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Clientes de la empresa.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-11.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10   
+                p.mb-0 Son las personas o empresas con las que se tiene relación y que les brindan confianza a los clientes.
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Datos de contacto.")
+            .row
+              .col-12.col-lg-2.mb-3.mb-lg-0
+                figure.justify-center
+                  img.img50(src='@/assets/curso/tema3/t3-12.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-10   
+                p.mb-0 Todas las formas de comunicación oral, visual o escrita que se puedan dar de la empresa.
+
+    h3 Portafolio impreso y digital
+
+    p.mb-5 El portafolio puede presentarse impreso o digital, esto depende de la necesidad que tenga la empresa de llegar al cliente.
+    
+    .row.justify-center.mb-5(data-aos="fade-down") 
+      .col-12.col-lg-8
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__texto.px-5.py-4.posit_1
+            p El mundo digital es utilizado cada vez con más frecuencia, por esto se recomienda que las empresas promuevan su portafolio de las dos formas para captar más clientes, en el caso de las microempresas como los restaurantes utilizan la carta impresa y una app (aplicación móvil que se puede escanear con un QR), facilitando al cliente la elección de los platos para hacer su pedido.
+          .bloque-texto-g__img.posit_2(
+            :style="{'background-image': `url(${require('@/assets/curso/tema3/t3-15.png')})`}") 
+    
+
 </template>
 
 <script>
